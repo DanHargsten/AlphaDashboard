81f75ae0c07a8e43d49e789ec6b4f89cd982a29f
Dan Hargsten <dan.hargsten@gmail.com>
2025-03-14 09:54:27 +0100

    Add project files.

---
+++ Alpha.Webb/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Alpha.Webb.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return LocalRedirect("/projects");
+            //return View();
+        }
+    }
+}
---
+++ Alpha.Webb/Controllers/ProjectsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Alpha.Webb.Controllers
+{
+    [Route("projects")]
+    public class ProjectsController : Controller
+    {
+        [Route("")]
+        public IActionResult Projects()
+        {
+            return View();
+        }
+    }
+}
