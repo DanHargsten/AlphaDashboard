26ec538b9da6d73c4822500205f8b94b2b30c49b
Dan Hargsten <dan.hargsten@gmail.com>
2025-03-21 10:39:27 +0100

    Backup

--- Alpha.Webb/Program.cs
+++ Alpha.Webb/Program.cs
@@ -10,7 +10,7 @@ app.UseAuthorization();
 app.MapStaticAssets();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Auth}/{action=Login}/{id?}")
+    pattern: "{controller=AuthController}/{action=Login}/{id?}")
     .WithStaticAssets();
 
 app.Run();
